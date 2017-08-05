ffd8487b36fb604e39d23f1bb1cab770b4e40b20
Scott Mason <sowasred2012@gmail.com>
2017-08-05 18:26:25 +0100

    add orbiting globe (in square orbit pattern)

--- WelcomeToVR/index.vr.js
+++ WelcomeToVR/index.vr.js
@@ -5,29 +5,186 @@ import {
   Pano,
   Text,
   View,
+  Sphere,
+  Animated,
+  Image
 } from 'react-vr';
+import Easing from 'react-native';
 
 export default class WelcomeToVR extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rotationValue: new Animated.Value(0),
+      orbitXValue: new Animated.Value(0),
+      orbitYValue: new Animated.Value(0),
+      orbitZValue: new Animated.Value(-3),
+    };
+  }
+
   render() {
+    var AnimatedSphere = Animated.createAnimatedComponent(Sphere)
+
     return (
       <View>
-        <Pano source={asset('outdoor.jpg')}/>
-        <Text
+        <Pano source={asset('coast.jpg')}/>
+        <AnimatedSphere 
+          radius={0.5}
+          widthSegments={20}
+          heightSegments={12}
+          texture={asset('globe.jpg')}
           style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
             layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          welcome
-        </Text>
+            transform: [
+              { translateX: this.state.orbitXValue },
+              { translateY: this.state.orbitYValue },
+              { translateZ: this.state.orbitZValue },
+              { rotateY: this.state.rotationValue },
+            ],
+          }}
+        />
       </View>
     );
+  }
+
+  componentDidMount() {
+    this.runAnimation1()
+  }
+
+  runAnimation1() {
+    this.state.rotationValue.setValue(0);
+    this.state.orbitXValue.setValue(0);
+    this.state.orbitYValue.setValue(0);
+    this.state.orbitZValue.setValue(-3);
+
+    Animated.timing(
+      this.state.rotationValue,
+      {
+        toValue: 360,
+        easing: Easing.linear,
+        duration: 10000,
+      } 
+    ).start();
+
+    Animated.timing(
+      this.state.orbitXValue,
+      {
+        toValue: 3,
+        easing: Easing.linear,
+        duration: 10000,
+      } 
+    ).start();
+
+    Animated.timing(
+      this.state.orbitZValue,
+      {
+        toValue: 0,
+        easing: Easing.linear,
+        duration: 10000,
+      } 
+    ).start(() => this.runAnimation2());
+  }
+
+  runAnimation2() {
+    this.state.rotationValue.setValue(0);
+    this.state.orbitXValue.setValue(3);
+    this.state.orbitYValue.setValue(0);
+    this.state.orbitZValue.setValue(0);
+
+    Animated.timing(
+      this.state.rotationValue,
+      {
+        toValue: 360,
+        easing: Easing.linear,
+        duration: 10000,
+      } 
+    ).start();
+
+    Animated.timing(
+      this.state.orbitXValue,
+      {
+        toValue: 0,
+        easing: Easing.linear,
+        duration: 10000,
+      } 
+    ).start();
+
+    Animated.timing(
+      this.state.orbitZValue,
+      {
+        toValue: 3,
+        easing: Easing.linear,
+        duration: 10000,
+      } 
+    ).start(() => this.runAnimation3());
+  }
+
+  runAnimation3() {
+    this.state.rotationValue.setValue(0);
+    this.state.orbitXValue.setValue(0);
+    this.state.orbitYValue.setValue(0);
+    this.state.orbitZValue.setValue(3);
+
+    Animated.timing(
+      this.state.rotationValue,
+      {
+        toValue: 360,
+        easing: Easing.linear,
+        duration: 10000,
+      } 
+    ).start();
+
+    Animated.timing(
+      this.state.orbitXValue,
+      {
+        toValue: -3,
+        easing: Easing.linear,
+        duration: 10000,
+      } 
+    ).start();
+
+    Animated.timing(
+      this.state.orbitZValue,
+      {
+        toValue: 0,
+        easing: Easing.linear,
+        duration: 10000,
+      } 
+    ).start(() => this.runAnimation4());
+  }
+
+  runAnimation4() {
+    this.state.rotationValue.setValue(0);
+    this.state.orbitXValue.setValue(-3);
+    this.state.orbitYValue.setValue(0);
+    this.state.orbitZValue.setValue(0);
+
+    Animated.timing(
+      this.state.rotationValue,
+      {
+        toValue: 360,
+        easing: Easing.linear,
+        duration: 10000,
+      } 
+    ).start();
+
+    Animated.timing(
+      this.state.orbitXValue,
+      {
+        toValue: 0,
+        easing: Easing.linear,
+        duration: 10000,
+      } 
+    ).start();
+
+    Animated.timing(
+      this.state.orbitZValue,
+      {
+        toValue: -3,
+        easing: Easing.linear,
+        duration: 10000,
+      } 
+    ).start(() => this.runAnimation1());
   }
 };
 
